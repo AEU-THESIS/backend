bc8c1a9cdc1c7de1fa3c84de7026b512ac361dcd
seangengise <seangengith@gmail.com>
2026-05-18 14:30:57 +0700

    feat(shop-settings): [AT-14] add admin routes for fetching and updating shop settings

--- src/core/Controller.ts
+++ src/core/Controller.ts
@@ -2,11 +2,12 @@
  * Core Controller Dependencies
  * Import this into every controller to avoid redundant boilerplate imports.
  */
-import { Request, Response, NextFunction } from "express";
-import { catchAsync } from "../utils/catchAsync";
-import { sendSuccess, sendError } from "../utils/apiResponse";
-import { HttpStatus } from "../constants/httpStatus";
-import { Messages } from "../constants/messages";
+import { Request, Response, NextFunction } from 'express'
+import { catchAsync } from '../utils/catchAsync'
+import { sendSuccess, sendError } from '../utils/apiResponse'
+import { AppError } from '../utils/appError'
+import { HttpStatus } from '../constants/httpStatus'
+import { Messages } from '../constants/messages'
 
 export {
   Request,
@@ -15,6 +16,7 @@ export {
   catchAsync,
   sendSuccess,
   sendError,
+  AppError,
   HttpStatus,
   Messages,
-};
+}
